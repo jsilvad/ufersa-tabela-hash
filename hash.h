b63a54f93ca2be1a53bedddfca00db25b6e13c87
Jefferson Silva <jefessh@outlook.com>
2024-03-24 18:58:55 -0300

    define estrutura e tamanho da tabela

--- hash.h
+++ hash.h
@@ -1,3 +1,5 @@
+#define TAMANHO_TABELA 10019  // Tamanho da tabela hash (número primo)
+
 typedef struct {
     char nome[50];
     char telefone[15];
@@ -8,3 +10,8 @@ typedef struct No {
     Contato contato;
     struct No *prox;
 } No;
+
+typedef struct {
+    No *vetor[TAMANHO_TABELA];
+    int tamanho;
+} TabelaHash;
