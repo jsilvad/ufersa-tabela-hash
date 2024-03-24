58b056a89e1f4d9100a75bab465ce92ded5c64fc
Jefferson Silva <jefessh@outlook.com>
2024-03-24 19:10:42 -0300

    inicializa as funções

--- hash.h
+++ hash.h
@@ -1,9 +1,12 @@
+#ifndef HASH_H
+#define HASH_H
+
 #define TAMANHO_TABELA 10019  // Tamanho da tabela hash (número primo)
 
 typedef struct {
-    char nome[50];
-    char telefone[15];
-    char email[40];
+    char nome[100];
+    char telefone[20];
+    char email[100];
 } Contato;
 
 typedef struct No {
@@ -13,5 +16,13 @@ typedef struct No {
 
 typedef struct {
     No *vetor[TAMANHO_TABELA];
-    int tamanho;
+    int tamanho;  // Tamanho atual da tabela
 } TabelaHash;
+
+void inicializarTabelaHash(TabelaHash *tabela);
+unsigned int funcaoHash(char *chave);
+void inserirContato(TabelaHash *tabela, Contato novoContato);
+No *buscarContato(TabelaHash tabela, char *chave);
+void removerContato(TabelaHash *tabela, char *chave);
+
+#endif
