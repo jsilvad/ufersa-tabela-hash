e93b43ded9717b73b52081c56d879e737823e904
Jefferson Silva <jefessh@outlook.com>
2024-03-25 16:09:50 -0300

    incrementa teste de remoção

--- main.c
+++ main.c
@@ -37,14 +37,23 @@ int main() {
     // }
 
     // Testando a inserção
-    Contato novoContato = {"Teste Nome", "Teste Telefone", "Teste Email"};
-    inserirContato(&tabela, novoContato);
-    No *resultadoBusca = buscarContato(tabela, "Teste Nome");
-    if (resultadoBusca != NULL) {
-        printf("Contato inserido com sucesso: %s, %s, %s\n", resultadoBusca->contato.nome, resultadoBusca->contato.telefone, resultadoBusca->contato.email);
-    } else {
-        printf("Falha ao inserir o contato.\n");
-    }
+    // Contato novoContato = {"Teste Nome", "Teste Telefone", "Teste Email"};
+    // inserirContato(&tabela, novoContato);
+    // No *resultadoBusca = buscarContato(tabela, "Teste Nome");
+    // if (resultadoBusca != NULL) {
+    //     printf("Contato inserido com sucesso: %s, %s, %s\n", resultadoBusca->contato.nome, resultadoBusca->contato.telefone, resultadoBusca->contato.email);
+    // } else {
+    //     printf("Falha ao inserir o contato.\n");
+    // }
+
+    // Testando a remoção
+    // removerContato(&tabela, "Teste Nome");
+    // resultadoBusca = buscarContato(tabela, "Teste Nome");
+    // if (resultadoBusca == NULL) {
+    //     printf("Contato removido com sucesso.\n");
+    // } else {
+    //     printf("Falha ao remover o contato.\n");
+    // }
 
     return 0;
 }
